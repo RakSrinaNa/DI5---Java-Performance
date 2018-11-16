248a49b9f31b1bd43b2380c7ab07fe8e6184d1ba
shadow.reyson <Rikudo777>
2018-11-16 10:14:34 +0100

    t

--- TP4/Code/src/main/java/fr/polytechtours/javaperformance/tp/tp4/Exercice3b.java
+++ TP4/Code/src/main/java/fr/polytechtours/javaperformance/tp/tp4/Exercice3b.java
@@ -1,20 +1,28 @@
 package fr.polytechtours.javaperformance.tp.tp4;
 
+import org.openjdk.jmh.annotations.*;
+import org.openjdk.jmh.infra.Blackhole;
+
 import java.util.ArrayList;
 import java.util.List;
 import java.util.concurrent.ExecutionException;
 import java.util.concurrent.ExecutorService;
 import java.util.concurrent.Executors;
 import java.util.concurrent.Future;
+import java.util.concurrent.atomic.AtomicInteger;
+import java.util.function.IntBinaryOperator;
 
 /**
  * Cet exercice est exactement le même que précédemment, sauf qu'une opération modulo est appliqué au compteur lors de chaque incrémentation.
  */
+@State(Scope.Benchmark)
 public class Exercice3b {
 
     private static final Object MUTEX = new Object();
 
     private Integer count = 0;
+    private int countA = 0;
+    private AtomicInteger countB = new AtomicInteger(0);
 
     private synchronized void incrementCounter(final Integer modulo) {
         synchronized (MUTEX) {
@@ -22,9 +30,28 @@ public class Exercice3b {
         }
     }
 
+    private void incrementCounterA(final Integer modulo) {
+        synchronized (MUTEX) {
+            countA = (countA + 1) % modulo;
+        }
+    }
+
     private void iterate(final Integer nbIteration, final Integer modulo) {
         for(Integer i = 0; i < nbIteration; i++) {
             this.incrementCounter(modulo);
+        }
+    }
+
+
+    private void iterateA(final int nbIteration, int modulo) {
+        for (int i = 0; i < nbIteration; i++) {
+            this.incrementCounterA(modulo);
+        }
+    }
+
+    private void iterateB(final int nbIteration, int modulo) {
+        for (int i = 0; i < nbIteration; i++) {
+            countB.accumulateAndGet(1, (left, right) -> (left + right) % modulo);
         }
     }
 
@@ -44,4 +71,68 @@ public class Exercice3b {
 
         return count;
     }
+
+
+    public int exercice3bA(final int nbThreads, final int nbIterationByThread, final int modulo) throws ExecutionException, InterruptedException {
+        final ExecutorService service = Executors.newFixedThreadPool(nbThreads);
+        final List<Future<?>> futures = new ArrayList<>();
+
+        for (int i = 0; i < nbThreads; i++) {
+            futures.add(service.submit(() -> iterateA(nbIterationByThread, modulo)));
+        }
+        service.shutdown();
+
+        // Wait for it...
+        for (final Future<?> future : futures) {
+            future.get();
+        }
+
+        return countA;
+    }
+
+    public int exercice3bB(final int nbThreads, final int nbIterationByThread, final int modulo) throws ExecutionException, InterruptedException {
+        final ExecutorService service = Executors.newFixedThreadPool(nbThreads);
+        final List<Future<?>> futures = new ArrayList<>();
+
+        for (int i = 0; i < nbThreads; i++) {
+            futures.add(service.submit(() -> iterateB(nbIterationByThread, modulo)));
+        }
+        service.shutdown();
+
+        for (Future<?> future : futures) {
+            future.get();
+        }
+
+        return countB.get();
+    }
+
+    //@Benchmark
+    @Warmup(iterations = 2, time = 1, batchSize = 3)
+    @Measurement(iterations = 2, time = 10, batchSize = 3)
+    public void test(Blackhole bh) throws ExecutionException, InterruptedException {
+        bh.consume(exercice3b(i, j, k).intValue());
+    }
+
+    @Benchmark
+    @Warmup(iterations = 2, time = 1, batchSize = 3)
+    @Measurement(iterations = 2, time = 10, batchSize = 3)
+    public void testA(Blackhole bh) throws ExecutionException, InterruptedException {
+        bh.consume(exercice3bA(i, j, k));
+    }
+
+    @Benchmark
+    @Warmup(iterations = 2, time = 1, batchSize = 3)
+    @Measurement(iterations = 2, time = 10, batchSize = 3)
+    public void testB(Blackhole bh) throws ExecutionException, InterruptedException {
+        bh.consume(exercice3bB(i, j, k));
+    }
+
+    @Param({"10", "100"})
+    private int i;
+
+    @Param({"10", "10000"})
+    private int j;
+
+    @Param({"10", "10000"})
+    private int k;
 }
--- TP4/Code/src/test/java/fr/polytechtours/javaperformance/tp/tp4/Exercice3bTest.java
+++ TP4/Code/src/test/java/fr/polytechtours/javaperformance/tp/tp4/Exercice3bTest.java
@@ -16,4 +16,24 @@ public class Exercice3bTest {
         Assert.assertEquals(40, new Exercice3b().exercice3b(100, 3000000, 260).intValue());
         Assert.assertEquals(2000, new Exercice3b().exercice3b(100, 5000000, 6000).intValue());
     }
+
+    @Test
+    public void testExercice3bA() throws ExecutionException, InterruptedException {
+        Assert.assertEquals(0, new Exercice3b().exercice3bA(10, 500, 100));
+        Assert.assertEquals(200, new Exercice3b().exercice3bA(7, 5000, 600));
+        Assert.assertEquals(100, new Exercice3b().exercice3bA(4, 100000, 300));
+        Assert.assertEquals(0, new Exercice3b().exercice3bA(5, 5000000, 100000));
+        Assert.assertEquals(40, new Exercice3b().exercice3bA(100, 3000000, 260));
+        Assert.assertEquals(2000, new Exercice3b().exercice3bA(100, 5000000, 6000));
+    }
+
+    @Test
+    public void testExercice3bB() throws ExecutionException, InterruptedException {
+        Assert.assertEquals(0, new Exercice3b().exercice3bB(10, 500, 100));
+        Assert.assertEquals(200, new Exercice3b().exercice3bB(7, 5000, 600));
+        Assert.assertEquals(100, new Exercice3b().exercice3bB(4, 100000, 300));
+        Assert.assertEquals(0, new Exercice3b().exercice3bB(5, 5000000, 100000));
+        Assert.assertEquals(40, new Exercice3b().exercice3bB(100, 3000000, 260));
+        Assert.assertEquals(2000, new Exercice3b().exercice3bB(100, 5000000, 6000));
+    }
 }
